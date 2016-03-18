aba8bb68bc7feaffae29845f5554b0b0a8e5e8f9
Diana Duong <dduong013@gmail.com>
2016-03-18 17:28:32 -0500

    added keypress on #userGuess

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
 
-  //Generate secret number between 1 and 100
+  //A function to generate a secret number between 1 and 100
     function  generateNumber (minimum, maximum) {
         return Math.floor((Math.random() * 100) + 1);
     }
@@ -19,59 +19,71 @@ $(document).ready(function(){
   	})
 
 
-  // Start new game
+  // A new game will start when clicking .new and will reset span#count, #userGuess, #feedback, and removing .newGuess. A secret number between 1 and 100 will be generated.
     $('.new').click(function() {
         console.log("***NEW GAME STARTED***")
         count = 0;
         $('span#count').text(count)
         $('#userGuess').val('')
         $('#feedback').text("Make your Guess!")
-        $('.newguess').remove()
+        $('.newGuess').remove()
         secretNumber = generateNumber(1,100)
         console.log("the secret number is " + secretNumber)
     })
 
-
-  //Every time #guessButton is clicked, the number of guesses are displayed in span#count
+ // When user inputs data within input#userGuess and clicks the #guessButton, the data is first checked to be a valid number, then the data is appended to ul#guessList inside an <li>. In addition, the number of guesses will increase the count and change the text to the current count number within span#count.
     $('#guessButton').click(function() {
         var number = $('#userGuess').val()
         var userGuess = parseInt(number)
         console.log("You guessed " + userGuess)
+        $('ul#guessList').append('<li class="newGuess">' + userGuess + '</li>')
 
         count += 1; 
         if (count > 0) {
             $('span#count').text(count)
-            $('ul#guessList').append('<li class="newguess">' + userGuess + '</li>')
         }
         $('#userGuess').val('')
-        userGuessFeedback(secretNumber, userGuess) //provides user feedback
-    }) 
-  
+        userGuessFeedback(secretNumber, userGuess) //user feedback
+    })
 
-  // Provide user feedback 
-function userGuessFeedback (secretNumber, userGuess) {
+  // When user inputs data within input#userGuess and presses the Enter key, the data is first checked to be a valid number, then the data is appended to ul#guessList inside an <li>. In addition, the number of guesses will increase the count and change the text to the current count number within span#count.
+    $('#userGuess').keypress(function (e) {
+        var number = $('#userGuess').val()
+        var userGuess = parseInt(number)
+
+        if (e.which == 13) {
+          if (userGuess) {          
+              console.log("You guessed " + userGuess)
+              $('ul#guessList').append('<li class="newGuess">' + userGuess + '</li>')
+        }
+        count += 1; 
+        if (count > 0) {
+            $('span#count').text(count)
+        }
+        $('#userGuess').val('')
+        userGuessFeedback(secretNumber, userGuess) //user feedback 
+        }
+      })
+
+  // Feedback will be provided to the user on a scale of Very Hot to Ice-Cold based on how close or how far out the user's guess is.
+  function userGuessFeedback (secretNumber, userGuess) {
     var feedback = secretNumber - userGuess;
     console.log("you are " + feedback + " number(s) away from the secret number");
 
     if (feedback === 0) {
-      $('#feedback').text("You Guessed It!");
-      console.log(text)
+       $('#feedback').text("You Guessed the Secret Number!");
     }
      else if (feedback <= 9 && feedback >= -9) { 
        $('#feedback').text("Very Hot") // between -9 to 9
-       console.log(text)
     }
     else if (feedback >= 10 && feedback <= 20 || feedback <= -10 && feedback >= -20)  {
        $('#feedback').text("Hot") // between 10 and 20 or between -10 and -20
-       console.log(text)
     }
      else if (feedback >= 21 && feedback <= 30 || feedback <= -21 && feedback >= -30) {
        $('#feedback').text("Warm") //between 21 and 30 or between -21 and -30
-       console.log(text)
     }
     else if (feedback >= 31 && feedback <= 49 || feedback <= -31 && feedback >= -49) {
        $('#feedback').text("Cold")// between 31 and 49 or between -31 and -49
-       console.log(text)
     }
     else {
        $('#feedback').text("Ice-Cold") // 50/-50 or further away
@@ -79,6 +91,7 @@ function userGuessFeedback (secretNumber, userGuess) {
   }
 
 }) // end ready function
+
 
 
 
